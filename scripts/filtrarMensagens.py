957b6d2782e6d020fa30fa50192a5adb29f28406
Guthierrez Gregorio <guthi.note@outlook.com>
2016-01-27 16:49:25 -0200

    Ajustes na filtragem de mensagem

--- scripts/filtrarMensagens.py
+++ scripts/filtrarMensagens.py
@@ -4,6 +4,7 @@
 __author__ = 'Guthierrez'
 
 import sys
+from unidecode import unidecode
 
 try:
     source = open(sys.argv[1], 'r')
@@ -12,34 +13,41 @@ try:
     stopwords = []
     for word in filestopwords.readlines():
         stopwords.append(word.rstrip())
+    stopwords = [unicode(x, 'utf-8') for x in stopwords]
 except IndexError:
     print('Argumentos inválidos, informe os arquivos de entrada e saída')
     sys.exit()
 
 lines = source.readlines()
 
+
 for index, line in enumerate(lines):
     if '-[' in line :
         
-        line = line.lower()
         line = line.replace('] ', ' '+str(index+1)+' ] ')
-
+        classe = line[:line.index(']')+1].rstrip('\n') #Classe e autor da mensagem
+        mensagem = line[line.index(']')+2:].rstrip('\n') #Conteúdo da mensagem
+        mensagem = unicode(mensagem, "utf-8")
+        mensagem = unidecode(mensagem)
+        mensagem = mensagem.lower()
+        
         #Remover caracteres especiais
-        for char in line:
-            if char in '!#.,();\'\"?:@':
-                line = line.replace(char, ' ')
+        for char in mensagem:
+            if char in '!#.,();\'\"?:@_/0123456789-[]|%+=':
+                mensagem = mensagem.replace(char, '')
         
         #Separando cada linha em palavras
-        words = line.split()
+        words = mensagem.split()
       
         #Retirando stopwords       
         for word in stopwords:
             if word in words:
                 while word in words:          
                     words.remove(word)
+            if 'http' in word or 'www' in word:
+                words.remove(word)
       
-        #Escrevendo a linha no novo arquivo
-        target.write(' '.join(words) + '\n')
+        target.write(classe + u' '.join(words).encode('utf-8').strip() + '\n')
 
 source.close()
 target.close()
